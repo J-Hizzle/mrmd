4c8f4b10ef30b072868016e211bb1d2c5f71725e
julianhille <julianhille@zedat.fu-berlin.de>
2025-06-17 17:42:42 +0200

    outsourced interpolation definitions to cpp file, Ref #047

--- mrmd/util/interpolation.hpp
+++ mrmd/util/interpolation.hpp
@@ -27,10 +27,7 @@ namespace util
  * @param factor interpolation factor in [0, 1]
  * @return interpolated value
  */
-real_t lerp(const real_t& left, const real_t& right, const real_t& factor)
-{
-    return left + (right - left) * factor;
-}
+real_t lerp(const real_t& left, const real_t& right, const real_t& factor);
 
 /**
  * Find the index of the first bin in the grid that is greater than or equal to the value.
@@ -38,12 +35,7 @@ real_t lerp(const real_t& left, const real_t& right, const real_t& factor)
  * @param value value to find
  * @return index of the right bin
  */
-idx_t findRightBin(const ScalarView& grid, const real_t& value)
-{
-    idx_t idx = 0;
-    for (; idx < idx_c(grid.extent(0)) && grid(idx) < value; ++idx);
-    return idx;
-}
+idx_t findRightBin(const ScalarView& grid, const real_t& value);
 
 /**
  * Interpolate data values of MultiHistogram to a new grid.
@@ -51,29 +43,7 @@ idx_t findRightBin(const ScalarView& grid, const real_t& value)
  * @param grid grid to interpolate to
  * @return MultiHistogram with interpolated values
  */
-data::MultiHistogram interpolate(const data::MultiHistogram& input, const ScalarView& grid)
-{
-    data::MultiHistogram output(
-        "interpolated-profile", input.min, input.max, grid.extent(0), input.numHistograms);
-    const ScalarView& inputGrid = input.createGrid();
-
-    auto policy = Kokkos::MDRangePolicy<Kokkos::Rank<2>>(
-        {idx_t(0), idx_t(0)}, {idx_c(grid.extent(0)), input.numHistograms});
-    auto kernel = KOKKOS_LAMBDA(const idx_t binIdx, const idx_t histogramIdx)
-    {
-        // find the two enclosing bins in the input histogram
-        auto rightBin = findRightBin(inputGrid, grid(binIdx));
-        auto leftBin = rightBin - 1;
-        output.data(binIdx, histogramIdx) =
-            lerp(input.data(leftBin, histogramIdx),
-                 input.data(rightBin, histogramIdx),
-                 (grid(binIdx) - inputGrid(leftBin)) * input.inverseBinSize);
-    };
-    Kokkos::parallel_for("MultiHistogram::interpolate", policy, kernel);
-    Kokkos::fence();
-
-    return output;
-}
+data::MultiHistogram interpolate(const data::MultiHistogram& input, const ScalarView& grid);
 
 }  // namespace util
 }  // namespace mrmd
