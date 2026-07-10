aec33279b27c35d7e73b62c1e61179f08f33d0f0
julianhille <julianhille@zedat.fu-berlin.de>
2026-07-10 17:57:05 +0200

    added non-symmetric slab utility

---
+++ mrmd/util/IsInSlab.hpp
@@ -0,0 +1,56 @@
+// Copyright 2024 Sebastian Eibl
+// Copyright 2026 Julian Friedrich Hille
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "datatypes.hpp"
+
+namespace mrmd
+{
+namespace util
+{
+class IsInSlab
+{
+private:
+    const real_t slabMin_;
+    const real_t slabMax_;
+    const AXIS axis_;
+    const real_t tolerance_;
+
+public:
+    KOKKOS_INLINE_FUNCTION
+    bool operator()(const real_t& x, const real_t& y, const real_t& z) const
+    {
+        auto coord = Point3D{x, y, z}[to_underlying(axis_)];
+
+        return (coord >= slabMin_ - tolerance_ && coord <= slabMax_ + tolerance_);
+    }
+
+    KOKKOS_INLINE_FUNCTION
+    bool operator()(const real_t& coord) const
+    {
+        return (coord >= slabMin_ - tolerance_ && coord <= slabMax_ + tolerance_);
+    }
+
+    IsInSlab(const real_t slabMin,
+             const real_t slabMax,
+             const AXIS axis = AXIS::X,
+             const real_t tolerance = 0_r)
+        : slabMin_(slabMin), slabMax_(slabMax), axis_(axis), tolerance_(tolerance)
+    {
+    }
+};
+}  // namespace util
+}  // namespace mrmd
